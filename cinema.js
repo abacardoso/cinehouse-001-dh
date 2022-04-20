cb191f8c29be785d366a8c4af772cf10c8fedb99
abacardoso <abacardoso@hotmail.com>
2022-04-19 22:01:25 -0300

    Function

--- cinema.js
+++ cinema.js
@@ -1,1 +1,11 @@
-console.log("Criamos o arquivo");
+var catalogo = require ("./database/catalogo.json")
+var catalogoString = JSON.stringify(catalogo) //Stringfy converte um objeto em string no JS, as pessoa susam para simplificar, tirar todos os "enters" e deixando numa linha só.
+
+var catalogoObj = JSON.parse(catalogoString) //Faz o contrário, devolve o formato para objeto normal.
+
+function listarTodosOsFilmes(filmes){
+    for(let i=0; i<filmes.lenght; i++){
+        console.log(filmes[i].titulo)
+    }
+}
+listarTodosOsFilmes(catalogo.data);
