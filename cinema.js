c0f07790e7db29cb59e63d621fb14c49ae7558ea
abacardoso <abacardoso@hotmail.com>
2022-04-26 21:31:26 -0300

    Add funcao aterarStatusEmCartaz

--- cinema.js
+++ cinema.js
@@ -3,6 +3,7 @@ var catalogoString = JSON.stringify(catalogo) //Stringfy converte um objeto em s
 
 var catalogoObj = JSON.parse(catalogoString) //Faz o contrário, devolve o formato para objeto normal.
 
+//LISTARTODOSOSFILMES:
 function listarTodosOsFilmes(filmes){
     for(let i=0; i<filmes.lenght; i++){
         console.log(filmes[i].titulo)
@@ -11,12 +12,35 @@ function listarTodosOsFilmes(filmes){
 listarTodosOsFilmes(catalogo.data);
 
 
+//LISTARFILMESEMCARTAZ:
+//Forma mais refatorada de fazer a função:
 function listarFilmesEmCartaz(filmes) {
     return filmes.filter((filme) => filme.emCartaz);
   }
-  console.log(listarFilmesEmCartaz(catalogoObj.data));
+console.log(listarFilmesEmCartaz(catalogoObj.data));
 
-  
-//OU ARROW FUNCTION:
+//Com arrow function:
 //const listarFilmesEmCartaz = (filmes) => filmes.filter((filme) => filme.emCartaz);
 
+
+//ALTERARSTATUSEMCARTAZ:
+function alterarStatusEmCartaz (id, filmes) {
+    const idxFilme = filmes.findIndex ((filme) => {
+        if (filme.codigo === id) {
+            return true;
+        }
+    });
+    if (idxFilme >= 0) {
+        filmes[idxFilme].emCartaz = !filme[idxFilme].emCartaz;
+    } else {
+        return false; //o else aqui funciona apenas para debugar, ter certeza que o código não vair dar erro
+    }
+}
+
+//Forma mais refatorada de fazer a função:
+function alterarStatusEmCartaz (id, filmes) {
+    const idxFilme = filmes.findIndex ((filmes) => filme.codigo === id);
+    if (idxFilme >= 0) {
+        filmes[idxFilme].emCartaz = !filmes[idxFilme].emCartaz
+    }
+}
